024cd06258e0ccb80dcb543dfb62618ba03b9e75
Лиза <zabrodimaliza2004@gmail.com>
2025-07-17 16:39:19 +0300

    first-commit

---
+++ javascript.js
@@ -0,0 +1,33 @@
+// 1. Создай три вложенных элемента (например, `div` внутри `div` внутри `div`).
+//  Назначь обработчики событий для каждого из них и проследи за последовательностью вызовов при клике на внутренний элемент с помощью console.log();
+document.getElementById('first').addEventListener("click", () => {
+    console.log('первый блок');
+});
+
+document.getElementById('second').addEventListener("click", (event) => {
+    console.log('второй блок');
+    event.stopPropagation(); // 2. Напиши код, который останавливает всплытие события на среднем элементе из предыдущего задания;
+});
+
+document.getElementById('third').addEventListener("click", () => {
+    console.log('третий блок');
+});
+
+// 3. Создай форму с несколькими полями ввода и кнопкой отправки. Реализуй делегирование события, 
+// например, валидации каждого поля ввода при его изменении. Пусть это будет простое условие, 
+// например, длина строки не более 20 символов.
+const button = document.getElementById('button');
+document.getElementById('inputs').addEventListener('input', (event) => {
+    if(event.target.tagName === 'INPUT') {
+        checkString(event.target);
+    }
+})
+
+function checkString(string) {
+    if (string.value.length > 5) { // 5 символов чтобы проверить быстрее
+        string.style.borderColor = 'red';
+        button.disabled = true;
+        return false
+    }
+    return true
+}
